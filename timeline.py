ce5fe8690b2c13080aa86c7dddd37171d7125f14
Maxim Ott <maxim.ott@gmx.de>
2024-01-23 12:36:53 +0100

    beta version

--- timeline.py
+++ timeline.py
@@ -11,28 +11,46 @@ from itertools import cycle
 from dodger import calculate_offsets
 import numpy as np
 
-# Prepare some things in advance
-# Read images
+######################################
+### Prepare some things in advance ###
+######################################
+# Default appearance settings
+global_settings = {
+    "year_font_size": 20,
+    "month_font_size": 16,
+    "date_dot_size": 10,
+    "relationship_line_width": 4,
+    "fplus_line_width": 2,
+    "dodge_step_size": 0.12,
+    "dodge_dates_days": 5
+}
+
+# Read symbol images
 symbols = {image_file: plt.imread(image_file) for image_file in glob.glob('symbols/*.png')}
-symbol_boxes = {key: OffsetImage(image, zoom=0.017) for key, image in symbols.items()}
+symbol_boxes = {key: OffsetImage(image, zoom=0.02) for key, image in symbols.items()}
 symbol_keys = [key for key, image in symbols.items()]
 symbol_key = cycle(symbol_keys)
 
+# Set color cycles
 bg_colors = [
     'xkcd:light pink',
     'xkcd:pale',
     'xkcd:pale blue',
     'xkcd:very pale green',
     'xkcd:egg shell',
-    'xkcd:pale peach'
+    'xkcd:pale peach',
+    'xkcd: light sky blue'
 ]
 bg_color = cycle(bg_colors)
 
 color_list = [
-    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
-    '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
-    '#9a6324', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-    '#000075', '#808080']
+    'dull purple', 'xkcd:umber', 'xkcd:golden yellow', 'xkcd:pale teal', 'xkcd:pinky red', # 5
+    'xkcd:bronze', 'xkcd:bluish', 'xkcd:vermillion', 'xkcd:sickly green', 'xkcd:cranberry', # 10
+    'xkcd:barney', 'xkcd:watermelon', 'xkcd:easter purple', 'xkcd:darkish green', 'xkcd:tomato', # 15
+    'xkcd:greeny blue', 'xkcd:pastel red', 'xkcd:topaz', 'xkcd:burple', 'xkcd:maroon', # 20 
+    'xkcd:olive', 'xkcd:forest green', 'xkcd:periwinkle', 'xkcd:lime', 'xkcd:lilac', # 25
+    'xkcd:royal purple', 'xkcd:light orange', 'xkcd:cerulean', 'xkcd:kelly green', 'xkcd:puke green', # 30
+]
 color = cycle(color_list)
 
 with st.sidebar:
@@ -171,8 +189,8 @@ with tab2:
         specials = st.session_state.specials
 
     specials_column_config = {
-        "special": st.column_config.TextColumn(label="Label", required=True),
-        "kind": st.column_config.TextColumn(label="Category", help="Usually FFM or MMF thresomes woud go here, but you can define your own categories", required=True),
+        "special": st.column_config.TextColumn(label="Label", help="Brief note to act as unique ID", required=True),
+        "kind": st.column_config.TextColumn(label="Category", help="Usually FFM or MMF thresomes would go here, but you can define your own categories", required=True),
         "start": st.column_config.DateColumn(
             "Date",
             help="""
@@ -184,6 +202,7 @@ with tab2:
             step=1,
             required=True
         ),
+        'participants': st.column_config.TextColumn(label="Participants", help="Participants separated by semicolons. Some fancy plotting options based on this might get added later.", required=False),
     }
     specials = st.data_editor(specials, use_container_width=True, column_config=specials_column_config, num_rows='dynamic')
 
@@ -237,17 +256,6 @@ with tab2:
 
 
 with tab3:
-    # Default appearance settings
-    global_settings = {
-        "year_font_size": 20,
-        "month_font_size": 16,
-        "date_dot_size": 10,
-        "relationship_line_width": 4,
-        "fplus_line_width": 2,
-        "dodge_step_size": 0.08,
-        "dodge_dates_days": 5
-    }
-
     "Here you can customize the general appearance of the graph."
     for key, value in global_settings.items():
         if isinstance(global_settings[key],int):
@@ -276,18 +284,7 @@ with tab4:
         "What kind of dates would you like to see in the graph?",
         ["All", "Kiss", "Sex"],
         captions = ["Show all dates.", "Show only dates where there was at least a kiss.", "Show only dates where you had sex."])
-
-    ########################
-    ### Persons Settings ###
-    ########################
-    st.markdown(
-        """
-        Here are all the individual people you have entered that you had a longterm arrangement or more than one date remaining after applying the filter.
-        For each of them you can set the following:
-        * Color: Use one of the named colors: https://matplotlib.org/stable/gallery/color/named_colors.html
-        * Offset: How much should the lines and dots be moved up (or down)
-        """
-    )
+    
     if show_dates == "All":
         filtered_df = df
         removed_people = [] 
@@ -297,11 +294,7 @@ with tab4:
     elif show_dates == "Sex":
         filtered_df = df[~((df.stage == "Kiss") | (df.stage == "No action"))] # This keeps the longterm stuff
         removed_people = list(df[(df.stage == "Kiss") | (df.stage == "No action")].person_name.unique())
-
-    colored_persons = list(filtered_df[filtered_df.kind == "Longterm"].person_name.unique())
-    colored_persons.extend(list(filtered_df[filtered_df.kind == "Date"].groupby("person_name").filter(lambda x: len(x) > 1).person_name.unique()))
-    ons = set(filtered_df.person_name) - set(colored_persons)
-
+    
     with st.expander("Some or all entries for these people will not be shown"):
         if len(removed_people)==0:
             st.write('No one here')
@@ -309,112 +302,147 @@ with tab4:
             for person in removed_people:
                 st.write(person)
         
-    with st.expander("These people will be colored grey"):
-        if len(ons)==0:
-            st.write('No one here')
-        else:
-            for person in ons:
-                st.write(person)
 
-
-    if 'person_settings' not in st.session_state:
-        person_settings = pd.DataFrame(columns=['person_name','facecolor', 'edgecolor'])
-        person_settings.person_name = list(set(colored_persons) | ons)
-        person_settings = person_settings.merge(
-            calculate_offsets(filtered_df, global_settings["dodge_dates_days"])[['person_name', 'offset']],
-            how = 'left',
-            on = 'person_name'
+    ########################
+    ### Persons Settings ###
+    ########################
+    with st.container(border=True):
+        st.markdown(
+            """
+            Here are all the individual people you have entered that you had a longterm arrangement or more than one date remaining after applying the filter.
+            For each of them you can set the following:
+            * Color: Use one of the named colors: https://matplotlib.org/stable/gallery/color/named_colors.html
+            * Offset: How much should the lines and dots be moved up (or down)
+            """
         )
+
+        colored_persons = list(filtered_df[filtered_df.kind == "Longterm"].person_name.unique())
+        colored_persons.extend(list(filtered_df[filtered_df.kind == "Date"].groupby("person_name").filter(lambda x: len(x) > 1).person_name.unique()))
+        ons = set(filtered_df.person_name) - set(colored_persons)
+
+        # with st.expander("These people will be colored grey"):
+        #     if len(ons)==0:
+        #         st.write('No one here')
+        #     else:
+        #         for person in ons:
+        #             st.write(person)
+
+
+        if 'person_settings' not in st.session_state:
+            person_settings = pd.DataFrame(columns=['person_name','facecolor', 'edgecolor'])
+            person_settings.person_name = list(set(colored_persons) | ons)
+            person_settings = person_settings.merge(
+                calculate_offsets(filtered_df, global_settings["dodge_dates_days"])[['person_name', 'offset']],
+                how = 'left',
+                on = 'person_name'
+            )
+            
+            person_settings.offset = person_settings.offset.fillna(0)
+
+            person_settings.facecolor = person_settings.facecolor.apply(lambda x: next(color))
+            person_settings.edgecolor = person_settings.facecolor
+
+            person_settings.loc[(person_settings['person_name'].isin(ons)), 'facecolor'] = 'grey'
+            person_settings.loc[(person_settings['person_name'].isin(ons)), 'edgecolor'] = 'grey'
         
-        person_settings.offset = person_settings.offset.fillna(0)
+            st.session_state.person_settings = person_settings
+        else:
+            person_settings = st.session_state.person_settings
 
-        person_settings.facecolor = person_settings.facecolor.apply(lambda x: next(color))
-        person_settings.edgecolor = person_settings.facecolor
+        person_settings_column_config = {
+            "person_name": st.column_config.TextColumn(label="Name", disabled = True),
+            "offset": st.column_config.NumberColumn(label="Offset", help="Leave empty to use a random value.", step=1),
+            "facecolor": st.column_config.TextColumn(label="Color", help="Leave empty to pick automatically.")
+        }
 
-        person_settings.loc[(person_settings['person_name'].isin(ons)), 'facecolor'] = 'grey'
-        person_settings.loc[(person_settings['person_name'].isin(ons)), 'edgecolor'] = 'grey'
-    
-        st.session_state.person_settings = person_settings
-    else:
-        person_settings = st.session_state.person_settings
-
-    person_settings_column_config = {
-        "person_name": st.column_config.TextColumn(label="Name", disabled = True),
-        "offset": st.column_config.NumberColumn(label="Offset", help="Leave empty to use a random value.", step=1),
-        "facecolor": st.column_config.TextColumn(label="Color", help="Leave empty to pick automatically.")
-    }
-
-    person_settings = st.data_editor(
-        person_settings, use_container_width=True,
-        column_config=person_settings_column_config,
-        column_order = ['person_name', 'facecolor', 'offset']
-    )
+        person_settings = st.data_editor(
+            person_settings, use_container_width=True,
+            column_config=person_settings_column_config,
+            column_order = ['person_name', 'facecolor', 'offset']
+        )
 
 
 
     #########################
     ### Specials Settings ###
     #########################
-    st.markdown(
-        """
-        Here are all the special activities. For each kind of them you can pick a symbol from a list of available emojis. In the second table you can adjust the individual offsets.
-        """
-    )
+    with st.container(border=True):
+        st.markdown(
+            """
+            Here are all the special activities. For each kind of them you can pick a symbol from a list of available emojis. In the second table you can adjust the individual offsets.
+            """
+        )
 
-   
-    if 'specials_summary' not in st.session_state:
-        specials_summary = specials.groupby('kind').size().reset_index(name='count').sort_values(by=['count', 'kind'], ascending = False).reset_index(drop=True)
-        
-        specials_summary['symbol_key'] = ""
-        # specials_summary['symbol_url'] = ""
+    
+        if 'specials_summary' not in st.session_state:
+            specials_summary = specials.groupby('kind').size().reset_index(name='count').sort_values(by=['count', 'kind'], ascending = False).reset_index(drop=True)
+            
+            specials_summary['symbol_key'] = ""
+            # specials_summary['symbol_url'] = ""
 
-        specials_summary.symbol_key = specials_summary.symbol_key.apply(lambda x: next(symbol_key))
-        # specials_summary.symbol_url = specials_summary.symbol_key.apply(lambda x: x)
-        specials_summary.edgecolor = "None"
+            specials_summary.symbol_key = specials_summary.symbol_key.apply(lambda x: next(symbol_key))
+            # specials_summary.symbol_url = specials_summary.symbol_key.apply(lambda x: x)
+            specials_summary.edgecolor = "None"
 
-        # Check for known participants
+            # Check for known participants
 
-        st.session_state.specials_summary = specials_summary
-    else:
-        specials_summary = st.session_state.specials_summary
+            st.session_state.specials_summary = specials_summary
+        else:
+            specials_summary = st.session_state.specials_summary
 
-    specials_settings_column_config = {
-        "kind": st.column_config.TextColumn(label="Kind", help="Self-defined kind of special activity", disabled = True),
-        "count": st.column_config.NumberColumn(label="Count", help="How many special activities of this (self-defined) kind you tracked.", disabled = True),
-        "symbol_key": st.column_config.SelectboxColumn(label="Symbol Key", help="Pick a symbol to represent this.", options=symbol_keys),
-        # "symbol_url": st.column_config.TextColumn(label="Symbol", width="small")
-    }
+        specials_summary_settings_column_config = {
+            "kind": st.column_config.TextColumn(label="Kind", help="Self-defined kind of special activity", disabled = True),
+            "count": st.column_config.NumberColumn(label="Count", help="How many special activities of this (self-defined) kind you tracked.", disabled = True),
+            "symbol_key": st.column_config.SelectboxColumn(label="Symbol Key", help="Pick a symbol to represent this.", options=symbol_keys),
+            # "symbol_url": st.column_config.TextColumn(label="Symbol", width="small")
+        }
 
-    specials_summary = st.data_editor(specials_summary, use_container_width=True, column_config=specials_settings_column_config)
+        specials_summary = st.data_editor(specials_summary, use_container_width=True, column_config=specials_summary_settings_column_config)
+    
+        if 'specials_offset' not in st.session_state:
+            specials_offset = specials[['special','kind','start','participants']]
+            
+            specials_offset['offset'] = 0
+            # specials_summary['symbol_url'] = ""
+
+            st.session_state.specials_offset = specials_offset
+        else:
+            specials_offset = st.session_state.specials_offset
+
+        specials_offset_settings_column_config = specials_column_config
+        specials_offset_settings_column_config['offset'] = st.column_config.NumberColumn(label="Offset", help="Modify if needed.", step = 1)
+
+        specials_offset = st.data_editor(specials_offset, use_container_width=True, column_config=specials_offset_settings_column_config)
 
     ##############################
     ### Circumstances Settings ###
     ##############################
-    st.markdown(
-        """
-        Here are all the circumstances you have described. For each of them you can set the following:
-        * Color: Use one of the named colors: https://matplotlib.org/stable/gallery/color/named_colors.html
-        """
-    )
+    with st.container(border=True):
+        st.markdown(
+            """
+            Here are all the circumstances (living abroad, etc.) you have described. For each of them you can set the following:
+            * Color: Use one of the named colors: https://matplotlib.org/stable/gallery/color/named_colors.html
+            """
+        )
 
-    if 'circumstances_summary' not in st.session_state:
-        circumstances_summary = circumstances.groupby('situation').size().reset_index(name='count').reset_index(drop=True)
-        
-        circumstances_summary['color'] = ""
+        if 'circumstances_summary' not in st.session_state:
+            circumstances_summary = circumstances.groupby('situation').size().reset_index(name='count').reset_index(drop=True)
+            
+            circumstances_summary['color'] = ""
 
-        circumstances_summary.color = circumstances_summary.color.apply(lambda x: next(bg_color))
+            circumstances_summary.color = circumstances_summary.color.apply(lambda x: next(bg_color))
 
-        st.session_state.circumstances_summary = circumstances_summary
-    else:
-        circumstances_summary = st.session_state.circumstances_summary
+            st.session_state.circumstances_summary = circumstances_summary
+        else:
+            circumstances_summary = st.session_state.circumstances_summary
 
-    circumstances_summary_column_config = {
-        "kind": st.column_config.TextColumn(label="Situation", disabled = True),
-        "count": st.column_config.NumberColumn(label="Count", help="How often this happened.", disabled = True),
-        "color": st.column_config.TextColumn(label="Color", help="You can change the color to any named matplotlib color or use the hex code."),
-    }
+        circumstances_summary_column_config = {
+            "situation": st.column_config.TextColumn(label="Situation", disabled = True),
+            "count": st.column_config.NumberColumn(label="Count", help="How often this happened.", disabled = True),
+            "color": st.column_config.TextColumn(label="Color", help="You can change the color to any named matplotlib color or use the hex code."),
+        }
 
-    circumstances_summary = st.data_editor(circumstances_summary, use_container_width=True, column_config=circumstances_summary_column_config)
+        circumstances_summary = st.data_editor(circumstances_summary, use_container_width=True, column_config=circumstances_summary_column_config)
 
 ###############
 #### GRAPH ####
@@ -481,9 +509,9 @@ with tab1:
     ### Specials
     # Merge symbols and alocate columns
     specials_df = specials.merge(specials_summary, on = "kind", how = "left")
+    specials_df = specials_df.merge(specials_offset[['special', 'offset']], on = "special", how = "left")
     specials_df['facecolor'] = 'grey'
     specials_df['edgecolor'] = specials_df.facecolor
-    specials_df['offset'] = 0
     specials_df['size'] = 9
 
     # Circumstances
@@ -511,7 +539,7 @@ with tab1:
                     linestyle=row.line_style,
                     lw = row.line_width,
                     color = row.facecolor,
-                    zorder = 0
+                    zorder = 0,
                 )
             elif year == row.start.year:
                 # Draw two lines so that the round caps dont' extend outside the area
@@ -561,7 +589,7 @@ with tab1:
                     linestyle=row.line_style,
                     lw = row.line_width,
                     color = row.facecolor,
-                    zorder = 0
+                    zorder = 0,
                 )
 
 
@@ -574,7 +602,8 @@ with tab1:
             facecolor = row.facecolor,
             edgecolor = row.edgecolor,
             marker = row.marker,
-            zorder = 1
+            zorder = 1, 
+            path_effects=[pe.Stroke(linewidth=2, foreground='k'), pe.Normal()]
         )
 
     # Add background circles for the special activities // Removed for now
@@ -650,3 +679,12 @@ with tab1:
             zorder = -99))
         
     st.pyplot(fig)
+    plt.savefig("mydatingtimeline.svg")
+
+    with open("mydatingtimeline.svg", "rb") as file:
+        btn = st.download_button(
+                label="Download as SVG",
+                data=file,
+                file_name="mydatingtimeline.svg",
+                mime="image/svg"
+            )
